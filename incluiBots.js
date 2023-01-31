f19dc628ecc324803f4b34df4049e6942784f193
edyleudo <edyleudo@hotmail.com>
2023-01-31 13:20:40 -0300

    primeiro commit

---
+++ incluiBots.js
@@ -0,0 +1,33 @@
+let original = [];
+var novoArray = [];
+let arrayBots = [5511999910621, 5521999816059, 556140040001, 551133350237, 5511974627106, 552140420321]
+let cont = 0;
+let vetorOriginal = document.getElementById("textNoBots").value
+
+
+function receberLista(){
+var ListaEnviada = document.getElementById("textNoBots").value;
+var qtdIntervalo = document.getElementById("qtd").value;
+console.log(qtdIntervalo);
+original = ListaEnviada.split("\n")
+
+for (let i = 0; i < original.length; i++) {
+
+    if (cont == qtdIntervalo) {
+        for (let j = 0; j < arrayBots.length; j++) {
+            novoArray.push(arrayBots[j].toString().substr(2));
+        }
+        cont = 0;
+        i--;
+    }
+    else {
+        cont++;  
+        novoArray.push(original[i].toString().substr(2));
+    }
+
+}
+
+
+document.getElementById("textBots").value = (novoArray);
+
+}
